f4f45e646d55fcbfee1c373b94d3284a77024378
Christelle de Boutray <christelle.de_boutray@etu.minesparis.psl.eu>
2024-11-12 16:42:09 +0100

    ajout de classes

--- playground.py
+++ playground.py
@@ -19,17 +19,70 @@ server = {
 }
 
 class User:
-    def _init_(self,id:int, name:str):
+    def __init__(self,id:int, name:str):
         self.name=name
         self.id=id
 
 
 user=User(1,'Alice')
+
+
+
+
 print(user.name)
 
-L_user=[]
+L_users=[]
 for user in server['users']:
     id=user['id']
     name=user['name']
     element=User(id,user)
-    L_user.append(element)
+    L_users.append(element)
+
+
+
+class Server:
+    def __init__(self,L_users,L_channels,L_messages):
+        self.users=L_users
+        self.channels=L_channels
+        self.messages=L_messages
+
+class Channel:
+    def __init__(self,id:int,member_ids:list,name:str):
+        self.id=id
+        self.name=name
+        self.member_ids=member_ids
+
+L_channels=[]
+for channel in server['channels']:
+    id=channel['id']
+    name=channel['name']
+    member_ids=channel['member_ids']
+    element=Channel(id,member_ids,name)
+    L_users.append(element)
+
+
+class Message:
+    def __init__(self,id:int,reception_date:str,channel:int,content):
+        self.id=id
+        self.reception_date=reception_date
+        self.channel=channel
+        self.content=channel
+
+L_messages=[]
+for message in server['messages']:
+    id=message['id']
+    reception_date=message['reception_date']
+    channel=message['channel']
+    content=message['content']
+    element=message(id,member_ids,name)
+    L_messages.append(element)
+
+
+
+class Server:
+    def __init__(self,L_users,L_channels,L_messages):
+        self.users=L_users
+        self.channels=L_channels
+        self.messages=L_messages
+
+server=Server(L_users,L_channels,L_messages)
