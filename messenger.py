7182988ef702c73bc70911d7736a46e03d7683e0
14alarro <14alarro>
2024-12-10 20:18:44 +0100

    Suppression du dictionnaire "server", chargé plus tard à partir du fichier json (ligne 9)

--- messenger.py
+++ messenger.py
@@ -1,25 +1,5 @@
 import json
 
-server = {
-    'users': [
-        {'id': 1, 'name': 'Alice'},
-        {'id': 2, 'name': 'Bob'}
-    ],
-    'channels': [
-        {'id': 1, 'name': 'Town square', 'member_ids': [1, 2]}
-    ],
-    'messages': [
-        {
-            'id': 1,
-            'reception_date': "2019-14-04",
-            'sender_id': 1,
-            'channel': 1,
-            'content': 'Hi '
-        }
-    ]
-}
-
-
 def ouverture_fichier():
     with open('server.json') as f:
         server=json.load(f)
