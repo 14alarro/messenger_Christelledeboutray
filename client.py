ccd1043206b681c286182d04ad3672cf2b4fa2e1
Christelle de Boutray <christelle.de_boutray@etu.minesparis.psl.eu>
2025-01-07 17:00:46 +0100

    save

---
+++ client.py
@@ -0,0 +1,51 @@
+
+
+
+class Client:
+    def __init__(self, server):
+        self.server=server
+    def __repr__(self):
+        print(f"Client(server={self.server})")
+    def welcome_screen(self):
+        server=self.server
+        def ecran_accueil():
+            print('=== Messenger ===')
+            print('1. See users')
+            print('2. See channel')
+            print('x.Leave')
+            print(' ')
+
+        def fonction_x():
+            print('Bye!')
+
+        def fonction_user():
+            print('Select an option: 1')
+            print('User list')
+            print('--------')
+            for user in server.users:
+                print("le nom est")
+                print(user.name)
+            
+            #for i in range(n):
+                #print(i,'.',server['users'][i]["name"])
+            print('n. Create user')
+            print('x. Main menu')
+
+
+        def fonction_channel():
+            for groupe in server.channels:
+                print(groupe.id,groupe.name)
+            print("x. main menu")
+            print("n. ajouter un groupe")
+            print('u. ajouter un utilisateur')
+
+
+        # max([mes['id'] for mess in server['messages]])+1
+        def fonction_add_user():
+            id=max([user.id for user in server.users])+1
+            nom=input("donner un nom d'utilisateur")
+            #modifier ici, mettre classe
+            new_user=User(id,nom)
+            server.users.append(new_user)
+            server.save()
+
