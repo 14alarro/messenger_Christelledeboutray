2bc52065c675ed08e129913cd989e64117e38243
Christelle de Boutray <christelle.de_boutray@etu.minesparis.psl.eu>
2025-01-14 17:03:24 +0100

    modif

--- remoteserver.py
+++ remoteserver.py
@@ -1,5 +1,5 @@
 # il faut que l'on ait une fonction remote server ici 
-
+# il faut faire un request.post 
 
 import requests
 import json
@@ -8,10 +8,13 @@ import json
 class RemoteServer:
     def __init__(self,url):
         self.server=requests.get(url)
+        self.url=url
     def get_users(self):
         response=self.server.json()
         L_users=[]
         for user in response:
             L_users.append(user["name"])
         return  L_users
+    def create_users(self):
+        #resquest.post )
     
