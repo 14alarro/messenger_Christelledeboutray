6bc0ae2b49cf0d59cde2875c62724f225de05230
Christelle de Boutray <christelle.de_boutray@etu.minesparis.psl.eu>
2025-01-14 16:39:38 +0100

    ajout de modifications

--- client.py
+++ client.py
@@ -8,45 +8,44 @@ class Client:
         print(f"Client(server={self.server})")
     def welcome_screen(self):
         server=self.server
-        ecran_accueil()
-        def ecran_accueil():
-            print('=== Messenger ===')
-            print('1. See users')
-            print('2. See channel')
-            print('x.Leave')
-            print(' ')
+    def ecran_accueil():
+        print('=== Messenger ===')
+        print('1. See users')
+        print('2. See channel')
+        print('x.Leave')
+        print(' ')
 
-        def fonction_x():
-            print('Bye!')
+    def fonction_x():
+        print('Bye!')
 
-        def fonction_user():
-            print('Select an option: 1')
-            print('User list')
-            print('--------')
-            for user in server.users:
-                print("le nom est")
-                print(user.name)
-            
-            #for i in range(n):
-                #print(i,'.',server['users'][i]["name"])
-            print('n. Create user')
-            print('x. Main menu')
-
-
-        def fonction_channel():
-            for groupe in server.channels:
-                print(groupe.id,groupe.name)
-            print("x. main menu")
-            print("n. ajouter un groupe")
-            print('u. ajouter un utilisateur')
+    def fonction_user(self):
+        print('Select an option: 1')
+        print('User list')
+        print('--------')
+        for user in self.server.users:
+            print("le nom est")
+            print(user.name)
+        
+        #for i in range(n):
+            #print(i,'.',server['users'][i]["name"])
+        print('n. Create user')
+        print('x. Main menu')
 
 
-        # max([mes['id'] for mess in server['messages]])+1
-        def fonction_add_user():
-            id=max([user.id for user in server.users])+1
-            nom=input("donner un nom d'utilisateur")
-            #modifier ici, mettre classe
-            new_user=User(id,nom)
-            server.users.append(new_user)
-            server.save()
+    def fonction_channel(self):
+        for groupe in self.server.channels:
+            print(groupe.id,groupe.name)
+        print("x. main menu")
+        print("n. ajouter un groupe")
+        print('u. ajouter un utilisateur')
+
+
+    # max([mes['id'] for mess in server['messages]])+1
+    def fonction_add_user(self):
+        id=max([user.id for user in server.users])+1
+        nom=input("donner un nom d'utilisateur")
+        #modifier ici, mettre classe
+        new_user=User(id,nom)
+        self.server.users.append(new_user)
+        self.server.save()
 
