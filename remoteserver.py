30484d28c109af4bdc6b92408c798696db69b796
Christelle de Boutray <christelle.de_boutray@etu.minesparis.psl.eu>
2025-01-14 16:33:38 +0100

    ajout fichier

---
+++ remoteserver.py
@@ -0,0 +1,17 @@
+# il faut que l'on ait une fonction remote server ici 
+
+
+import requests
+import json
+
+
+class RemoteServer:
+    def __init__(self,url):
+        self.server=requests.get(url)
+    def get_users(self):
+        response=self.server.json()
+        L_users=[]
+        for user in response:
+            L_users.append(user["name"])
+        return  L_users
+    
