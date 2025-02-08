6bca3bbd8acb2c45dad246a0a8b12e10c395acf5
Christelle de Boutray <christelle.de_boutray@etu.minesparis.psl.eu>
2025-02-08 19:58:39 +0100

    modif client

--- client.py
+++ client.py
@@ -1,18 +1,22 @@
 
 from model import User
-#méthode post 
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
 
 
 class Client:
     def __init__(self, server):
         self.server=server
+
     def __repr__(self):
         print(f"Client(server={self.server})")
+
     @staticmethod
     def ecran_accueil():
         print('=== Messenger ===')
         print('1. See users')
         print('2. See channel')
+        print("3. See messages")
         print('x.Leave')
         print(' ')
 
@@ -30,17 +34,18 @@ class Client:
         print('n. Create user')
         print('x. Main menu')
 
-
     def fonction_channel(self):
-        for groupe in self.server.channels:
-            print(groupe.id,groupe.name)
+        server=self.server
+        print("la liste des groupes est",server.get_channels())
         print("x. main menu")
-        print("n. ajouter un groupe")
-        print('u. ajouter un utilisateur')
+        print("n. créer un groupe")
+        print("m. voir les membres d'un groupe")
+        print('u. ajouter un utilisateur à un groupe existant')
 
-
-    # max([mes['id'] for mess in server['messages]])+1
-
+    def fonction_messages(self):
+        print(self.server.get_messages())
+        print('n create messages')
+        print('x. Main menu')
 
     def welcome_screen(self):
         self.ecran_accueil()
@@ -65,18 +70,38 @@ class Client:
                 choice=input('select an option')
                 while choice!='x':
                     if choice == 'n':
-                        self.fonction_groupe()
-                    if choice=='u':
-                        self.ajout_membre_groupe()
+                        nom=input("donne moi le nom du groupe que tu veux créer")
+                        self.server.create_channel(nom)
+                    elif choice=='u':
+                        channel=input("donne moi le nom du groupe auquel tu veux ajouter quelqu'un")
+                        user=input("donne moi le nom de la personne que tu veux ajouter au groupe")
+                        self.server.join_group(channel,user)
+                    elif choice=='m':
+                        pass
+                    else:
+                        print("entrer une commande répertoriée")
                     choice=input('select an option')
                 self.ecran_accueil()
+
+            elif choice=='3':
+                self.fonction_messages()
+                choice=input('select an option')
+                while choice!='x':
+                    if choice == 'n':
+                        channel=input("donne moi le nom du groupe sur lequel tu veux poster un message")
+                        sender=input("donne moi le nom de la personne qui envoie le message")
+                        content=input("donne moi message que tu veux poster")
+                        self.server.post_messages(channel,content,sender)
+        
+                    else:
+                        print('taper une commande répertoriée')
+                    choice=input('select an option')
             elif choice =='x':
                 print('Bye') 
             else:
-                input("taper une commande répertoriée")
+                choice=input("taper une commande répertoriée")
+            self.ecran_accueil()
             choice = input('Select an option: ')
         
 
 
-#il faut ajouter des fonctions pour rejoindre un groupe, voir la liste des membres du groupe ...
-#il faut une fonction pour obtenir à partir d'un 
