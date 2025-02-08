e61df07e10b6ae87535f1861bbc090f9d3dbc465
Christelle de Boutray <christelle.de_boutray@etu.minesparis.psl.eu>
2025-02-08 19:59:35 +0100

    modif remoteserver

--- remoteserver.py
+++ remoteserver.py
@@ -1,6 +1,3 @@
-# il faut que l'on ait une fonction remote server ici 
-# il faut faire un request.post 
-
 import requests
 import json
 from model import User
@@ -17,13 +14,16 @@ class RemoteServer:
         return  L_users
     def add_users(self,nom):
         requests.post(f"{self.url}/users/create",json={"name": nom} )
+    def create_channel(self,nom):
+        requests.post(f"{self.url}/channels/create",json={"name": nom} )
     def get_channels(self):
         reponse=requests.get(f"{self.url}/channels")
         L=reponse.json()
         L_channels=[]
         for channel in L:
             L_channels.append(channel["name"])
-    def add_channel(self):
+        return L_channels
+    def add_channel(self,nom):
         requests.post(f"{self.url}/channels/create",json={"name": nom} )
     def get_messages(self):
         reponse=requests.get(f"{self.url}/messages")
@@ -32,23 +32,50 @@ class RemoteServer:
         for message in L:
             L_messages.append(message["content"])
         return  L_messages
-    def join_group(self,channel_id,user_id):
-        requests.post(f"{self.url}/{channel_id}/create",json={"user_id": user_id} )
-    def post_messages(self, channel_id, content, sender_id):
+    def get_channel_id(self,channel):
+        reponse=requests.get(f"{self.url}/channels")
+        L=reponse.json()
+        i=0
+        n=len(L)
+        while i<n and L[i]['name']!=channel:
+            i+=1
+        if i==n:
+            print("groupe inexistant")
+            channel=input("donner moi un groupe qui existe")
+            self.get_channel_id(channel)
+        else:
+            channel_id=L[i]['id']
+        return(L[i]['id'])
+    def get_user_id(self,user):
+        reponse=requests.get(f"{self.url}/users")
+        L=reponse.json()
+        i=0
+        n=len(L)
+        while i<n and L[i]['name']!=user:
+            i+=1
+        if i==n:
+            print("utilisateur inexistant")
+            user=input("donner moi un utilisateur qui existe")
+            self.get_user_id(user)
+        else:
+            user_id=L[i]['id']
+        return(user_id)
+    def join_group(self,channel,user):
+        channel_id=self.get_channel_id(channel)
+        user_id=self.get_user_id(user)
+        requests.post(f"{self.url}/channels/{channel_id}/join",json={"user_id": user_id} )
+    def see_members_group(channel,self):
+        channel_id=self.get_channel_id(channel)
+        reponse=requests.get(f"{self.url}/channels/{channel_id}/members").json()
+        L=[]
+        for user in reponse:
+            L.append(user['name'])
+        print(L)
+    def post_messages(self, channel, content, sender):
+        channel_id=self.get_channel_id(channel)
+        sender_id=self.get_user_id(sender)
         requests.post(f"{self.url}/{channel_id}/messages/post",json={"sender_id": sender_id, "content": content} )
 
 
 
-#modifier les fonctions de manière à avoir les noms des groupes que l'on manipule
-#utiliser notion override et héritage
-# que fait @abstractmethod: documentation
-
-#server contient toutes les méthodes avec des pass
-#si on n'a pas une version de python qui est assez avancé pour mettre override, on ne va rien mettre
-
-    
-
-
-#response=requests.get("https://groupe5-python-mines.fr/users")
-#print(response.json())
 
