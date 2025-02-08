812dcd23275de046c979477089ede3ed83cb3434
Christelle de Boutray <christelle.de_boutray@etu.minesparis.psl.eu>
2025-02-08 19:58:57 +0100

    modif du serveur local

--- localserver.py
+++ localserver.py
@@ -1,7 +1,9 @@
 import json 
 from model import Message, Channel,User
+from datetime import datetime
 
-# faire ça partout
+
+
 
 class LocalServer:
     def __init__(self,LocalServer_FILE_NAME):
@@ -32,6 +34,13 @@ class LocalServer:
             self.users=L_users
             self.channels=L_channels
             self.messages=L_messages
+
+    def create_channel(self,nom):
+        id=max([channel.id for channel in self.channels])+1
+        new_user=User(id,nom)
+        self.channels.append(new_user)
+        self.save()
+
     def __repr__(self):
         return(f"LocalServer(users={self.users},channels={self.channels}, messages={self.messages}")
     def save(self):
@@ -45,28 +54,69 @@ class LocalServer:
         L_messages=[message.to_dict() for message in self.messages]
         serveur["messages"]=L_messages
         with open('LocalServer.json', 'w') as f:
-            json.dump(serveur, f,indent=10) 
+            json.dump(serveur, f,indent=10)
+
+    def get_user_name(self,id):
+        L_users=self.users
+        n=len(L_users)
+        while i<n and L_users[i].id!=id:
+            i+=1
+        if i==n:
+            print("cette personne n'existe pas")
+            id=input("donne moi une personne qui existe")
+            self.get_users_name(self,id) 
+        return(L_users[i].name)
+    def get_user_id(self,user):
+        L_users=self.users
+        n=len(L_users)
+        while i<n and L_users[i].name!=user:
+                i+=1
+        if i==n:
+                print("cette personne n'existe pas")
+                user=input("donne moi une personne qui existe")
+                self.get_users_id(self,user)
+        return(L_users[i].id)
+    def get_channel_id(self,channel):
+        L_channels=self.channels
+        n=len(L_channels)
+        while i<n and L_channels[i].name!=channel :
+                i+=1
+        if i==n:
+                print("ce groupe n'existe pas")
+                channel=input("donne moi un groupe qui existe")
+                self.get_users_id(self,channel)
+        return(L_channels[i].id)
     def get_users(self):
         L_users=[]
         for user in self.users:
             L_users.append(user.name)
-        return ("la liste est",L_users)
+        return (L_users)
     def get_messages(self):
         L_messages=[message.to_dict() for message in self.messages]
         return (L_messages)
     def get_channels(self):
         L_channels=[channel.to_dict() for channel in self.channels]
         print(L_channels)
-    def join_group(self,channel,user):
-        L_users=self.users
-        i=0
-        while L_users[i].name!=user and i<len(L_users):
-                print(L_users[i].name)
-                i=i+1
-        if i==(len(L_users)):
-                print("cette personne n'existe pas")
+    def see_members_group(channel,self):
+        L_channels=self.channels
+        j=0
+        n=len(L_channels)
+        while j<n and L_channels[j].name!=channel:
+            j+=1
+        if j==n:
+            print("ce groupe n'existe pas")
+            channel=input("donner moi un groupe qui existe")
+            self.see_members_group(channel)
         else:
-            member_id=L_users[i].id  
+            L_ids=L_channels[j].member_ids
+            L_names=[]
+            for id in L_ids:
+                L_names.append(self.get_user_name(id))
+            print(L_names)
+
+            
+    def join_group(self,channel,user):
+        member_id=self.get_user_id(user)  
         j=0
         L_channels=self.channels
         while L_channels[j].name!=channel:
@@ -77,15 +127,15 @@ class LocalServer:
             L_channels[j].member_ids.append(member_id)
             self.channels=L_channels
         self.save()
-    def post_messages(self,channel,post):
-        i=0
-        L_messages=self.message
-        while L_messages[i].channel!=channel:
-            j=j+1
-        if j==(len(L_messages)+1):
-            print("ce groupe n'existe pas")
-        else:
-            L_messages
-        
 
-#il ne faut pas oublier d'enregistrer le serveur local quand on fait des modifications
+    def post_messages(self, channel, content, sender):
+         channel_id=self.get_channel_id(channel)
+         sender_id=self.get_user_id(sender)
+         reception_date=str(datetime.now())
+         id = max([message.id for message in self.messages])+1
+         new_message=Message(self,id,reception_date,sender_id, channel_id,content)
+         L_messages=self.messages
+         L_messages.append(new_message)
+         self.messages=L_messages
+         self.save()
+        
